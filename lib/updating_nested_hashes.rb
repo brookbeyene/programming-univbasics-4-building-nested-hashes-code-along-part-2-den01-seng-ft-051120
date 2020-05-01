e2316dbf345eb8c5ab3e1cfbb46598077ef3a6b8
Brook Beyene <brookbeyene37@gmail.com>
2020-05-01 07:07:41 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -17,7 +17,8 @@ def update_status
          {name: "Steven", age: "30", attitude: "confused"},
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
-      }
+      },
+      :
    }
 
   # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
